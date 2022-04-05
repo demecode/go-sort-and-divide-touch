4171402fa923a4ae85922c8e9584f52b5d1dae71
demecode <dpk.williams@gmail.com>
2022-04-05 14:06:26 +0100

    recursion added and base case

--- sort.go
+++ sort.go
@@ -1,11 +1,24 @@
 package main
 
-import "ftm"
+import "fmt"
 
-func findMaxSubArray(nums []int, left int right int) int{
+func findMaxSubArray(nums []int, left int,right int) int{
 
-	// this will return the actual max sub array of the three segments
-	return max(maxLeft, maxRight, middleCross)
+	// create a base case to finish to recursion
+	if left == right {
+		return nums[left]
+	} else {
+	// perform recurssion for the three segments
+	mid := (left + right) / 2
+		maxLeft := findMaxSubArray(nums, left, right, mid)
+		maxRight := findMaxSubArray(nums, mid+1, right)
+
+		// cross fucntion for a max sub arraybetween the left, mid and right
+		cross := maxCross(nums, left, mid, right)
+
+		// this will return the actual max sub array of the three segments
+		return max(maxLeft, maxRight, middleCross)
+	}
 }
 
 func main(){
